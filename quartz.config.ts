5fd824ac4210d0df7bc72b550f2ff556d5ef7812
Rauze <2656423861@qq.com>
2024-08-29 01:41:18 +0800

    Quartz sync: Aug 29, 2024, 1:41 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Rauze Learning",
+    pageTitle: "💻 Rauze Learning",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
