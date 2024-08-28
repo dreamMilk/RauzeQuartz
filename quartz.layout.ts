04ca010db1a5f1952526113923187b340d1df495
Rauze <2656423861@qq.com>
2024-08-29 01:07:07 +0800

    Quartz sync: Aug 29, 2024, 1:07 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -32,9 +32,9 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
+    // Component.Graph(),
+    // Component.DesktopOnly(Component.TableOfContents()),
+    // Component.Backlinks(),
   ],
 }
 
