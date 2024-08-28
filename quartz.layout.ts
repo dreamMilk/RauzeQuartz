eb56f460db46cc05550ba5297aec6e87dcf2a8a6
Rauze <2656423861@qq.com>
2024-08-29 01:24:50 +0800

    Quartz sync: Aug 29, 2024, 1:24 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,9 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [
-    Component.Explorer()
-  ],
+  afterBody: [],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/dreamMilk/RauzeQuartz",
@@ -32,9 +30,9 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    // Component.Graph(),
-    // Component.DesktopOnly(Component.TableOfContents()),
-    // Component.Backlinks(),
+    Component.Graph(),
+    Component.DesktopOnly(Component.TableOfContents()),
+    Component.Backlinks(),
   ],
 }
 
